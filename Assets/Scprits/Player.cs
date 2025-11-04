43272600043c7f7a34f5245ead9740d35995bfe0
kienvu15 <88vutrunkien@gmail.com>
2025-11-04 11:51:56 +0700

    up

---
+++ Assets/Scprits/Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
